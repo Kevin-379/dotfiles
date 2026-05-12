274b7f653eff7711bcaf93370a74e67fc8cb08f8
Kevin Shah <skevin@uber.com>
2026-05-12 11:39:34 +0530

    add nvim md plugin

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'jakewvincent/mkdnflow.nvim',
+    ft = { 'markdown', 'rmd' },
+    opts = {
+      mappings = {
+        MkdnEnter = { { 'i', 'n', 'v' }, '<CR>' }, -- also handle Enter in insert mode (new list items, table rows)
+        MkdnFoldSection = false,   -- frees <leader>f (used by conform: format buffer)
+        MkdnToggleToDo = false,    -- frees <C-Space> (used by blink.cmp: open menu)
+        MkdnTableNextCell = false, -- frees insert <Tab> (used by blink.cmp: snippet expand)
+        MkdnTablePrevCell = false, -- frees insert <S-Tab>
+      },
+    },
+  },
+}
